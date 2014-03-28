eddb5ba9671522a07e1946bb7483feed93f09180
Lewis Liu <develop@lewisl.net>
2014-03-28 15:32:02 -0400

    -adding missing column class

---
+++ src/java/Model/Column.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Model;
+
+/**
+ *
+ * @author Lewis
+ */
+
+public class Column {
+    private String id;    
+    private String label;
+    private String type;
+    private boolean isDefault;
+    
+    public Column(String id, String label, String type, boolean isDefault) {
+        this.id = id;
+        this.label = label;
+        this.type = type;
+        this.isDefault = isDefault;
+    }
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isDefault() {
+        return isDefault;
+    }
+
+    public void setIsDefault(boolean isDefault) {
+        this.isDefault = isDefault;
+    }
+}
